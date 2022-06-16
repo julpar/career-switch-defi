cb01400a725434dbb72d22c88107f886d801087c
alezvi <alezvi@users.noreply.github.com>
2022-06-16 17:40:36 -0300

    Update test.php

--- test.php
+++ test.php
@@ -1,43 +1,19 @@
 <?php
 
-/**
- * Esta funcion emula el comportamiento de la API
- *
- * @param int $x
- * @param int $y
- * @return boolean
- */
-function isNext($x,$y) {
-    /**
-     * Puedes cambiar esta lista si quieres probar otros ejemplos
-     * Siempre debes respetar que sea un array de strings
-     * El resto de la funcionalidad se mantiene igual
-     */
-    $original = [
-        "abc", "def", "ghi",
-    ];
-
-    $flipped = array_flip($original);
-    $xIndex = $flipped[$x];
-    $yIndex = $flipped[$y];
-
-    return ($xIndex+1) == $yIndex;
-}
-
 function check($blocks = []) {
     /**
      * Escribe aqui tu solucion leyendo el argumento que recibe
      * Y retorna el resultado correctamente ordenado
      * Segun el ejemplo provisto, seria...
      */
-    return ["abc", "def", "ghi"];
+    return ["f319", "46ec", "c1c7", "3720", "c7df", "c4ea", "4e3e", "80fd"];
 }
 
 // Enviamos datos desordenados para testear el resultado
-$result = check(["abc", "ghi", "def"]);
+$result = check(["f319", "3720", "4e3e", "46ec", "c7df", "c1c7", "80fd", "c4ea"], "b93ac073-eae4-405d-b4ef-bb82e0036a1d");
 
 // Esperamos que el resultado sea como este array
-$expected = ['abc', 'def', 'ghi'];
+$expected = ["f319", "46ec", "c1c7", "3720", "c7df", "c4ea", "4e3e", "80fd"];
 
 if ($result == $expected) {
     echo "Lo resolviste correctamente!";
