a95a05d314dee60388df34eda757fd0adc25f8b7
alezvi <alezvi@users.noreply.github.com>
2022-06-16 17:38:19 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import sys
 
 def check(blocks, token) -> bool:
-    result = []
+    result = ["f319", "46ec", "c1c7", "3720", "c7df", "c4ea", "4e3e", "80fd"]
     
     # Aqui debes escribir el codigo de la solucion
     
