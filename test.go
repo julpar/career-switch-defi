130f3343fb9217d98a3a35d3be0fc0f7783a9937
alezvi <alezvi@users.noreply.github.com>
2022-06-16 17:51:09 -0300

    Update test.go

--- test.go
+++ test.go
@@ -5,41 +5,51 @@ import (
 	"reflect"
 )
 
-// Esta funcion emula el comportamiento de la API
-func isNext(x string, y string) bool {
-	var blocks map[string]int
-	blocks = make(map[string]int)
-
+func check(blocks map[string]string, token string) map[string]string {
 	/**
-     * Puedes cambiar esta lista si quieres probar otros ejemplos
-     * Siempre debes respetar que sea un array de strings
-     * El resto de la funcionalidad se mantiene igual
-     */
-	blocks["abc"] = 0
-	blocks["def"] = 1
-	blocks["ghi"] = 2
-
-	return blocks[x]+1 == blocks[y]
-}
-
-func check(blocks map[string]string) map[string]string {
-	/**
-     * Escribe aqui tu solucion leyendo el argumento que recibe
-     * Y retorna el resultado correctamente ordenado
-     * Segun el ejemplo provisto, seria...
-     */
+	 * Escribe aqui tu solucion leyendo el argumento que recibe
+	 * Y retorna el resultado correctamente ordenado
+	 * Segun el ejemplo provisto, seria...
+	 */
 	return map[string]string{
-		"0": "abc",
-		"1": "def",
-		"2": "ghi",
+		"0": "f319",
+		"1": "46ec",
+		"2": "c1c7",
+		"3": "3720",
+		"4": "c7df",
+		"5": "c4ea",
+		"6": "4e3e",
+		"7": "80fd",
 	}
 }
 
 func main() {
+	/**
+	 * Lo que esperamos recibir
+	 */
 	expected := map[string]string{
-		"0": "abc",
-		"1": "def",
-		"2": "ghi",
+		"0": "f319",
+		"1": "46ec",
+		"2": "c1c7",
+		"3": "3720",
+		"4": "c7df",
+		"5": "c4ea",
+		"6": "4e3e",
+		"7": "80fd",
+	}
+	
+	/**
+	 * Secuencia de bloques desordenados
+	 */
+	blocks := map[string]string{
+		"0": "f319",
+		"1": "3720",
+		"2": "4e3e",
+		"3": "46ec",
+		"4": "c7df",
+		"5": "c1c7",
+		"6": "80fd",
+		"7": "c4ea",
 	}
 
 	result := check(expected)
